f8f3da8e0efa3bfd4932f771f29710bf5d317a85
Shrishail <shrishailgajbhar5@gmail.com>
2020-07-23 22:08:06 +0530

    Perform 5 fold training and validation

--- src/train.py
+++ src/train.py
@@ -1,14 +1,14 @@
-# Let us train the model using RandomForestClassifier and ExtraTreeClassifier
 import os
 import pandas as pd
 from sklearn import ensemble
 from sklearn import preprocessing
 from sklearn import metrics
-from . import dispatcher
-
 import joblib
 
+from . import dispatcher
+
 TRAINING_DATA = os.environ.get("TRAINING_DATA")
+TEST_DATA = os.environ.get("TEST_DATA")
 FOLD = int(os.environ.get("FOLD"))
 MODEL = os.environ.get("MODEL")
 
@@ -22,7 +22,7 @@ FOLD_MAPPPING = {
 
 if __name__ == "__main__":
     df = pd.read_csv(TRAINING_DATA)
-    
+    df_test = pd.read_csv(TEST_DATA)
     train_df = df[df.kfold.isin(FOLD_MAPPPING.get(FOLD))].reset_index(drop=True)
     valid_df = df[df.kfold==FOLD].reset_index(drop=True)
 
@@ -37,7 +37,7 @@ if __name__ == "__main__":
     label_encoders = {}
     for c in train_df.columns:
         lbl = preprocessing.LabelEncoder()
-        lbl.fit(train_df[c].values.tolist() + valid_df[c].values.tolist())
+        lbl.fit(train_df[c].values.tolist() + valid_df[c].values.tolist() + df_test[c].values.tolist())
         train_df.loc[:, c] = lbl.transform(train_df[c].values.tolist())
         valid_df.loc[:, c] = lbl.transform(valid_df[c].values.tolist())
         label_encoders[c] = lbl
@@ -48,5 +48,6 @@ if __name__ == "__main__":
     preds = clf.predict_proba(valid_df)[:, 1]
     print(metrics.roc_auc_score(yvalid, preds))
 
-    joblib.dump(label_encoders, f"models/{MODEL}_label_encoder.pkl")
-    joblib.dump(clf, f"models/{MODEL}.pkl")
+    joblib.dump(label_encoders, f"models/{MODEL}_{FOLD}_label_encoder.pkl")
+    joblib.dump(clf, f"models/{MODEL}_{FOLD}.pkl")
+    joblib.dump(train_df.columns, f"models/{MODEL}_{FOLD}_columns.pkl")
