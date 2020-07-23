e55aba9cc634c9513305c1ff897ee7f5c655725e
Shrishail <shrishailgajbhar5@gmail.com>
2020-07-23 19:26:17 +0530

    Perform stage-1 of ML framework: Training and validation

---
+++ src/dispatcher.py
@@ -0,0 +1,5 @@
+from sklearn import ensemble
+MODELS = {
+'randomforest':ensemble.RandomForestClassifier(n_estimators=200,n_jobs=-1, verbose=2),
+'gradientboost':ensemble.GradientBoostingClassifier(n_estimators=200,verbose=2)
+}
